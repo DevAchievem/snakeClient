738825c761ee54b2211894a27f7016d27b023a74
Mathius Johnson <devachievem@gmail.com>
2020-07-29 13:49:44 -0700

    exit key

--- play.js
+++ play.js
@@ -1,3 +1,27 @@
 const connect = require('./client');
 console.log('Connecting ...');
+
+/**
+ * Setup User Interface
+ * Specifically, so that we can handle user input via stdin
+ */  const stdin = process.stdin;
+
+const setupInput = function() {
+  handleUserInput();
+
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function()  {
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+
+};
+setupInput();
 connect();
